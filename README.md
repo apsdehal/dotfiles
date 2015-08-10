468d9837c8bc779852c2ce949baae5d39fa0a407
Amanpreet Singh <apsdehal@gmail.com>
2015-08-10 18:47:37 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dotfiles
+
+__I will need Dotfiles they said, but I will need them 100 times they never said.__
---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Install Vim and Git
+sudo apt-get install vim git curl
+
+## LAMP Stack
+sudo apt-get install apache2 php5 mysql-server libapache2-mod-auth-mysql php5-mysql libapache2-mod-php5 php5-mcrypt php5-curl phpmyadmin
+
+### Enable PHP Mcrypt extension
+sudo phpenmod mcrypt
+
+## Install Terminator
+sudo add-apt-repository -y ppa:gnome-terminator
+sudo apt-get update
+sudo apt-get -y install terminator
